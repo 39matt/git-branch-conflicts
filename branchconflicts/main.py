afb28cedba57a103b14ec6959640fc3a0facffdc
39matt <matija.todosijevic.i4@gmail.com>
2025-04-06 20:31:24 +0200

    add: get latest commit (remote and local)

--- branchconflicts/main.py
+++ branchconflicts/main.py
@@ -42,6 +42,34 @@ def find_branch_by_name_local(local_repo_path:str, branch_name:str) -> Branch:
 
     return Branch(branch_name, last_line.split(" ")[1])
 
+def get_latest_commit_remote(branch: Branch, owner:str, repo:str) -> Commit:
+    api_call = f"https://api.github.com/repos/{owner}/{repo}/commits/" + branch.commit_sha
+    response = requests.get(api_call).json()
+    commit = Commit(response["commit"]["message"], response["sha"], [])
+    for parent in response["parents"]:
+        commit.parents_sha.append(parent["sha"])
+
+    return commit
+
+def get_latest_commit_local(branch: Branch, local_repo_path:str) -> Commit:
+    commit = Commit("","",[])
+    with open(local_repo_path + "/.git/logs/refs/heads/" + branch.name, "r") as branch_file:
+        branch_file.readline()
+        for line in branch_file:
+            fields = line.split(" ")
+            print(fields[0] + "\n")
+            commit.parents_sha.append(fields[0])
+        commit.sha = fields[1]
+        commit.message = ' '.join(fields[6:])
+
+    return commit
+
+def find_merge_base(branch_a: Branch, branch_b: Branch, repo_owner:str, repo_name:str, local_repo_path:str) -> str:
+    latest_commit_remote = get_latest_commit_remote(branch_a, repo_owner, repo_name)
+    latest_commit_local = get_latest_commit_local(branch_b, local_repo_path)
+    print(latest_commit_remote, latest_commit_local)
+    return ""
+
 if __name__ == "__main__":
     github_username = "39matt"
     repository_name = "jetbrains-test-repo"
